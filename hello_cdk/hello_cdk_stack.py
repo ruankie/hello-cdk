f2e134dea4df7d52d81b17aa534dbf95e80af653
Ruan Pretorius <ruan@melio.ai>
2025-03-03 16:39:13 +0200

    Define hello world lambda

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,19 +1,30 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,  # Import the Lambda module
 )
 from constructs import Construct
+
 
 class HelloCdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "HelloCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Define the Lambda function resource
+        my_function = _lambda.Function(
+            self,
+            id="HelloWorldFunction",  # ID within the CDK app
+            # function_name="HelloWorldFunction",  # The actual name of the function (optional)
+            runtime=_lambda.Runtime.PYTHON_3_12,
+            handler="index.handler",
+            # memory_size=512,  # MB
+            code=_lambda.Code.from_inline(
+                """
+def handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': 'Hello World!'
+    }
+                """
+            ),
+        )

8a263a6d32b4f8057263abadb431ebff0052154b
Ruan Pretorius <ruan@melio.ai>
2025-03-03 16:22:39 +0200

    Set AWS region and account info in env vars

--- app.py
+++ app.py
@@ -5,24 +5,15 @@ import aws_cdk as cdk
 
 from hello_cdk.hello_cdk_stack import HelloCdkStack
 
+AWS_ACCOUNT_ID = os.environ.get("AWS_ACCOUNT")
+AWS_REGION = os.environ.get("AWS_REGION")
+
 
 app = cdk.App()
-HelloCdkStack(app, "HelloCdkStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+HelloCdkStack(
+    app,
+    "HelloCdkStack",
+    env=cdk.Environment(account=AWS_ACCOUNT_ID, region=AWS_REGION),
+)
 
 app.synth()

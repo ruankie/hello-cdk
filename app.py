e309a651ab093108233d2d55bc4d04e8c304ae43
Ruan Pretorius <ruan@melio.ai>
2025-03-03 16:29:23 +0200

    Load AWS config from env vars

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import os
-
+from dotenv import load_dotenv
 import aws_cdk as cdk
-
 from hello_cdk.hello_cdk_stack import HelloCdkStack
 
+# Config
+load_dotenv()
 AWS_ACCOUNT_ID = os.environ.get("AWS_ACCOUNT")
 AWS_REGION = os.environ.get("AWS_REGION")
 
